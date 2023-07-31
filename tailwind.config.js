76f36c51c86d0a1939f34ef660eb52da8f71ff6b
mryaxyobek <pubgn9642@gmail.com>
2023-07-31 14:36:01 +0500

    site is optimized for devices up to 370px

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      'media-500': '500px',
+      'media-370': '370px',
+      'media-300': '300px',
+    },
     extend: {},
   },
   plugins: [],
